bc6578ac8faffb8ff21bc35f7627cae36bfc670b
Karim <kael63804@eleve.isep.fr>
2024-08-27 14:53:38 +0200

    petite modification...

--- karim.py
+++ karim.py
@@ -2,16 +2,21 @@ import random
 
 print("Trouver Le prix de l'objet X")
 cible = random.randint(1, 10)
-print(f"cible={cible}")
+#print(f"cible={cible}")
 
-essai = int(input("votre proposition entre 1 et 10"))
+for i in range(5):
+ # Récupération de la proposition du joueur
+ essai = int(input("votre proposition entre 1 et 10"))
 
-if essai < 1 or essai > 10:
-    print("erreur il faut saisir une valeur entre 1 et 10")
-    exit()
-if cible == essai:
-    print("bravo")
-elif cible < essai:
-    print("trop élevé")
-else :
+ if essai < 1 or essai > 10:
+     print("erreur il faut saisir une valeur entre 1 et 10")
+     exit()
+ if cible == essai:
+     print("bravo")
+     break
+ elif cible < essai:
+     print("trop élevé")
+ else :
     print("trop peu")
+
+print("Fin de jeu")
